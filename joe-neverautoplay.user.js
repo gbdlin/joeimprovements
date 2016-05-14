60672d3d237b313ac1b2fe8ebb339c7c8bb5df30
GwynBleidD <gbdlin@users.noreply.github.com>
2016-05-14 15:35:54 +0200

    Create joe-neverautoplay.user.js

---
+++ joe-neverautoplay.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name NeverEverAutoplay for JoeMonster TV
+// @description Wyłącza wkurzający autoplay w MonsterTV
+// @include http://*.joemonster.org/filmy/*
+// ==/UserScript== 
+
+
+window.autoplayYTMovie = function() {};
+window.autoplayNextMovie = function() {};
+
+$('.mtv-autoplay-switch').remove();
+$('.mtv-autoplay-label').html('Autoodtwarzanie zablokowane przez NeverEverAutoplay');
